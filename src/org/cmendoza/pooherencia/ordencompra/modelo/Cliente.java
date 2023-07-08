49b69d0468a55df342c7eb689805519f4750bf06
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2023-07-08 17:53:53 -0600

    se creo el constructor y los metodos gtter para la clase cliente

--- src/org/cmendoza/pooherencia/ordencompra/modelo/Cliente.java
+++ src/org/cmendoza/pooherencia/ordencompra/modelo/Cliente.java
@@ -5,6 +5,16 @@ public class Cliente {
     private String apellido;
 
     //////// CONSTRUCTOR /////////////
+    public Cliente(String nombre,String apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
     /////// GETTER AND SETTER ///////
+    public String getNombre(){
+        return this.nombre;
+    }
+    public String getApellido(){
+        return this.apellido;
+    }
     ////// METODOS /////////////////
 }
