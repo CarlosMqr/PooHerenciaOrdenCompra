8ca333bf5b9150199aa62a37a5ce460e3dcabf4d
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2023-07-08 17:49:28 -0600

    se creo el contructor y los metodos getter para la clase producto

--- src/org/cmendoza/pooherencia/ordencompra/modelo/Producto.java
+++ src/org/cmendoza/pooherencia/ordencompra/modelo/Producto.java
@@ -5,6 +5,21 @@ public class Producto {
     private int precio;
     private String fabricante;
     //////// CONSTRUCTOR /////////////
+    public Producto (String fabricante, String nombre, int precio){
+        this.fabricante = fabricante;
+        this.nombre = nombre;
+        this.precio = precio;
+
+    }
     /////// GETTER AND SETTER ///////
+    public String getNombre(){
+        return this.nombre;
+    }
+    public String getFabricante(){
+        return this.fabricante;
+    }
+    public int getPrecio(){
+        return this.precio;
+    }
     ////// METODOS /////////////////
 }
